3f4529ffcb364cdb308c032413fc129c9cfa92af
Engleek <engleek@engleek.com>
2010-06-30 15:58:54 +0200

    Added valid/invalid pips before tab labels: new tab widget.

--- formulaBook.ml
+++ formulaBook.ml
@@ -1,15 +1,35 @@
 open Tools
+open FormulaEditor
+
+class tabWidget () =
+
+  let hbox = GPack.hbox () in
+
+  let validityIcon = GMisc.image
+    ~file:"rc/bullet_green.png"
+    ~icon_size:`MENU
+    ~packing:hbox#add () in
+
+  let label = GMisc.label
+    ~text:"Formule"
+    ~packing:hbox#add () in
+    
+    object (self)
+      inherit GObj.widget hbox#as_widget
+      
+      method label () = label
+      
+      method set_valid valid () =
+        if valid
+        then validityIcon#set_stock `NO
+        else validityIcon#set_stock `NO
+    end
 
 class formulaBook ?packing ?show () =
 
   (* Notebook and Tabs *)
   let notebook = GPack.notebook
-    ~tab_border:0
     ?packing ?show () in
-
-  (* Adding Tab *)
-  let addImage = GButton.tool_button
-    ~stock:`ADD () in
 
     object (self)
       inherit GObj.widget notebook#as_widget
@@ -19,9 +39,10 @@ class formulaBook ?packing ?show () =
 
       method notebook = notebook;
 
-      method add = notebook;
+      (*method newFormula data = 
+        notebook#prepend_page ~tab_label:(GMisc.label ~text:"Formule B" ())#coerce (new formulaEditor ())#coerce;*)
 
       initializer
-        ignore (notebook#append_page ~tab_label:addImage#coerce (GMisc.label ~text:"This is an empty page, you shouldn't be seeing it..." ())#coerce);
+        ignore (notebook#prepend_page ~tab_label:((new tabWidget ())#coerce) (new formulaEditor ())#coerce);
         (*addImage#connect#clicked (fun () -> print "New Formula Button clicked!");*)
     end
