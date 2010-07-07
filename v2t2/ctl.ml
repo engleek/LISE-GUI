41c8363e24b091a4de97cb6937ce8b3eec936a61
v2t2 <nautilea@yahoo.fr>
2010-07-07 17:33:44 +0200

    ajout fonction de satisfaction

--- v2t2/ctl.ml
+++ v2t2/ctl.ml
@@ -20,3 +20,57 @@ walk_formula =
 | Diamond of walk_formula 
 | Square of walk_formula
 
+
+
+let satisfies_vp s phi = true 
+let rec satisfies_sf kp s phi =
+  match
+    phi
+  with  
+      True -> true 
+    | False -> false
+    | VP(x) ->  satisfies_vp s phi
+    | Neg (psi )-> not (satisfies_sf kp s psi) 
+    | Or(psi1, psi2) -> (satisfies_sf kp s psi1) || (satisfies_sf kp s psi2)
+    | And(psi1, psi2) -> (satisfies_sf kp s psi1) && (satisfies_sf kp s psi2)
+    | Implies(psi1, psi2) ->( (not (satisfies_sf kp s psi1))  || (satisfies_sf kp s psi2))
+    | Equiv (psi1, psi2) -> (satisfies_sf kp s (Implies (psi1, psi2))) && (satisfies_sf kp s (Implies( psi2, psi1))) 
+  (*  | A(psi) ->
+	begin  
+	  try 
+	    List.for_all (fun x -> satisfies_wf kp x psi) (Kripke.liste_suivants s kp )
+	  with
+	      Not_found ->  failwith "à voir la def de A dans la relation de satisfaction"
+	end
+	  
+    | E(psi) ->
+	begin 
+	  try
+	    List.exists  (fun x -> satisfies_wf kp x psi) (Kripke.nextstate s kp )
+	  with Not_found ->  failwith "à voir la def de E dans la relation de satisfaction"
+	end
+
+and  satisfies_wf kp s phi =
+  match 
+    phi
+  with  
+    | Next(psi) -> 
+	begin 
+	  try 
+	    List.for_all  (fun x -> satisfies_sf kp x psi) (Kripke.nextstate s kp )
+	  with Not_found ->  failwith "à voir la def de NEXT dans la relation de satisfaction"
+	end
+	  
+    |Square (psi ) ->
+       begin  
+	 let a = (satisfies_wf kp s (Next(psi))) in
+	   if not a then false else  (List.forall (fun x-> satisfies_wf kp x (Square psi) ) (Kripke.nextstate s kp))
+       end
+
+    |Diamond (psi ) -> 
+       begin 
+	 let a = (satisfies kp s (Next(psi))) in
+	   if  a then true else  (List.exists (fun x-> satisfies_wf kp x (Square psi) ) (Kripke.nextstate s kp))
+       e 
+  *)
+    |_ -> failwith "a virer"
