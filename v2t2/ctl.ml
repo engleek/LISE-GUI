3045e3b4facd14834712f10d854fb5b1fb95a4e6
v2t2 <nautilea@yahoo.fr>
2010-07-08 11:35:16 +0200

    changement type des formules et fonction de satisfaction

--- v2t2/ctl.ml
+++ v2t2/ctl.ml
@@ -1,44 +1,46 @@
 (* le type des variables propositionnelles *)
 
-type var_prop = (*hum mis pour l'exemple, sans doute a améliorer*)  string 
+type var_prop =  string 
 
-type state_formula = 
+
+type formula = 
     True 
   | False 
-  | Neg of state_formula 
+  | Neg of formula 
   | VP of var_prop 
-  | Or of state_formula * state_formula 
-  | And of state_formula * state_formula
-  | Implies of state_formula * state_formula
-  | Equiv of state_formula * state_formula
-  | A of walk_formula 
-   |E of walk_formula
+  | Or of formula * formula 
+  | And of formula * formula
+  | Implies of formula * formula
+  | Equiv of formula * formula
+  | A of formula 
+   |E of formula
+   | Next of formula 
+| Diamond of formula 
+| Square of formula
 
-and 
-walk_formula =
- Next of state_formula 
-| Diamond of walk_formula 
-| Square of walk_formula
+
 
 
 
 let satisfies_vp s phi = true 
-let rec satisfies_sf kp s phi =
+
+
+let rec satisfies (kp:Kripke.kripke) (s:Kripke.state) phi =
   match
     phi
   with  
       True -> true 
     | False -> false
     | VP(x) ->  satisfies_vp s phi
-    | Neg (psi )-> not (satisfies_sf kp s psi) 
-    | Or(psi1, psi2) -> (satisfies_sf kp s psi1) || (satisfies_sf kp s psi2)
-    | And(psi1, psi2) -> (satisfies_sf kp s psi1) && (satisfies_sf kp s psi2)
-    | Implies(psi1, psi2) ->( (not (satisfies_sf kp s psi1))  || (satisfies_sf kp s psi2))
-    | Equiv (psi1, psi2) -> (satisfies_sf kp s (Implies (psi1, psi2))) && (satisfies_sf kp s (Implies( psi2, psi1))) 
-  (*  | A(psi) ->
+    | Neg (psi )-> not (satisfies kp s psi) 
+    | Or(psi1, psi2) -> (satisfies kp s psi1) || (satisfies kp s psi2)
+    | And(psi1, psi2) -> (satisfies kp s psi1) && (satisfies kp s psi2)
+    | Implies(psi1, psi2) ->( (not (satisfies kp s psi1))  || (satisfies kp s psi2))
+    | Equiv (psi1, psi2) -> (satisfies kp s (Implies (psi1, psi2))) && (satisfies kp s (Implies( psi2, psi1))) 
+    | A(psi) ->
 	begin  
 	  try 
-	    List.for_all (fun x -> satisfies_wf kp x psi) (Kripke.liste_suivants s kp )
+	    List.for_all (fun x -> satisfies kp x psi) (Kripke.nextstate s kp )
 	  with
 	      Not_found ->  failwith "à voir la def de A dans la relation de satisfaction"
 	end
@@ -46,31 +48,29 @@ let rec satisfies_sf kp s phi =
     | E(psi) ->
 	begin 
 	  try
-	    List.exists  (fun x -> satisfies_wf kp x psi) (Kripke.nextstate s kp )
+	    List.exists  (fun x -> satisfies kp x psi) (Kripke.nextstate s kp )
 	  with Not_found ->  failwith "à voir la def de E dans la relation de satisfaction"
 	end
 
-and  satisfies_wf kp s phi =
-  match 
-    phi
-  with  
-    | Next(psi) -> 
+    | Next(psi) ->
 	begin 
 	  try 
-	    List.for_all  (fun x -> satisfies_sf kp x psi) (Kripke.nextstate s kp )
+	    List.for_all  (fun x -> satisfies kp x psi) (Kripke.nextstate s kp )
 	  with Not_found ->  failwith "à voir la def de NEXT dans la relation de satisfaction"
 	end
 	  
     |Square (psi ) ->
        begin  
-	 let a = (satisfies_wf kp s (Next(psi))) in
-	   if not a then false else  (List.forall (fun x-> satisfies_wf kp x (Square psi) ) (Kripke.nextstate s kp))
+	 let a = (satisfies kp s (Next(psi))) in
+	   if not a then false else  (List.for_all (fun x-> satisfies kp x (Square psi) ) (Kripke.nextstate s kp))
        end
 
     |Diamond (psi ) -> 
        begin 
 	 let a = (satisfies kp s (Next(psi))) in
-	   if  a then true else  (List.exists (fun x-> satisfies_wf kp x (Square psi) ) (Kripke.nextstate s kp))
-       e 
-  *)
-    |_ -> failwith "a virer"
+	   if  a then true else  (List.exists (fun x-> satisfies kp x (Square psi) ) (Kripke.nextstate s kp))
+     end
+
+
+
+(*ATTENTION ECRIRE UNE FONCTION QUI VERIFIE QU'UNE FORMULE DONNEE EST BIEN EN CTL !!!!!!! *)
