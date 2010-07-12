964078dd7990ef81fe6831bba45f326ca7a69982
Engleek <engleek@engleek.com>
2010-07-12 11:37:32 +0200

    Connected add buttons

--- mainWindow.ml
+++ mainWindow.ml
@@ -40,9 +40,18 @@ class mainWindow ?(show=false) () =
   (* Formula General Toolbar Buttons *)
   let formulaSpacer = GButton.separator_tool_item
     ~packing:(fun w -> toolbar#insert w) () in
-  let newFormulaButton = GButton.tool_button
-    ~stock:`ADD
+  let addVPButton = GButton.tool_button
     ~packing:(fun w -> toolbar#insert w) () in
+  let addFormulaButton = GButton.tool_button
+    ~packing:(fun w -> toolbar#insert w) () in
+
+  (* Icons *)
+  let addFormulaIcon = GMisc.image
+    ~file:"rc/plugin_add.png"
+    ~icon_size:`MENU () in
+  let addVPIcon = GMisc.image
+    ~file:"rc/pill_add.png"
+    ~icon_size:`MENU () in
 
   (* Tooltips *)
   let tooltips = GData.tooltips () in
@@ -60,8 +69,6 @@ class mainWindow ?(show=false) () =
   (* Log View *)
   let logView = new logView
     ~packing:vpaned#add2 () in
-
-  let tab = new tabWidget () in
 
     object (self)
     
@@ -93,22 +100,26 @@ class mainWindow ?(show=false) () =
         window#connect#destroy
           ~callback:Main.quit;
 
+        (* Button setup *)
+        addVPButton#set_icon_widget (addVPIcon)#coerce;
+        addFormulaButton#set_icon_widget (addFormulaIcon)#coerce;
+
         (* Toolbar Sigs *)
         newButton#connect#clicked self#newFormula;
         openButton#connect#clicked self#loadFormula;
         saveButton#connect#clicked self#saveFormula;
+        addVPButton#connect#clicked ~callback:(fun () -> formulaBook#newVP ~name:"name" ~content:"name");
+        addFormulaButton#connect#clicked ~callback:(fun () -> formulaBook#newFormula ~name:"name" ~content:"name");
 
         (* Tooltips *)
         tooltips#set_tip ~text:string_new_tooltip newButton#coerce;
         tooltips#set_tip ~text:string_open_tooltip openButton#coerce;
         tooltips#set_tip ~text:string_save_tooltip saveButton#coerce;
-        tooltips#set_tip ~text:string_add_formula_tooltip newFormulaButton#coerce;
+        tooltips#set_tip ~text:string_add_formula_tooltip addFormulaButton#coerce;
 
         (* Customizations *)
         toolbar#set_icon_size `SMALL_TOOLBAR;
         vpaned#set_position (mainHeight - (mainHeight / 3));
-
-        tab#set_valid false;
 
         (* Curtains! *)
         if show then window#show ();
